4bcb242ddb3afc677fb917cd04e9dfffebb0a2d0
Billy4J <18062102798@163.com>
2022-08-11 15:58:54 +0800

    [feat] sanic中使用async方法，使其能够并发

--- web/sanic-demo/app.py
+++ web/sanic-demo/app.py
@@ -1,7 +1,7 @@
+import asyncio
+
 from sanic import Sanic
 from sanic.response import text
-import cv2
-import numpy
 
 app = Sanic(__name__)
 
@@ -11,10 +11,13 @@ async def handler(request):
     print(request)
     return text('POST request ')
 
+
 @app.get('/')
 async def handler(request):
-    print(request)
-    return text('POST request ')
+    print("得到请求")
+    await asyncio.sleep(5)
+    return text('hello')
+
 
 if __name__ == '__main__':
-    app.run("0.0.0.0",5678)
+    app.run("0.0.0.0", 5678)
